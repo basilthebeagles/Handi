2501ec73b6e76d02ebe65421dd60555f89fa41e3
basilthebeagles <johnjoestack@gmail.com>
2015-08-25 14:09:14 +0100

    fixed iAd

--- Handi/iAdViewController.swift
+++ Handi/iAdViewController.swift
@@ -9,29 +9,45 @@
 import UIKit
 import iAd
 
-class iAdViewController: UIViewController {
-
+class iAdViewController: UIViewController, ADBannerViewDelegate {
+    
+    @IBOutlet var adBannerView: ADBannerView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.canDisplayBannerAds = true
         
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        self.canDisplayBannerAds = true
+        self.adBannerView.delegate = self
+        self.adBannerView.hidden = true //hide until ad loaded
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func bannerViewWillLoadAd(banner: ADBannerView!) {
+        NSLog("bannerViewWillLoadAd")
     }
-    */
-
+    
+    func bannerViewDidLoadAd(banner: ADBannerView!) {
+        NSLog("bannerViewDidLoadAd")
+        self.adBannerView.hidden = false //now show banner as ad is loaded
+    }
+    
+    func bannerViewActionDidFinish(banner: ADBannerView!) {
+        NSLog("bannerViewDidLoadAd")
+        
+        //optional resume paused game code
+        
+    }
+    
+    func bannerViewActionShouldBegin(banner: ADBannerView!, willLeaveApplication willLeave: Bool) -> Bool {
+        NSLog("bannerViewActionShouldBegin")
+        
+        //optional pause game code
+        
+        return true
+    }
+    
+    func bannerView(banner: ADBannerView!, didFailToReceiveAdWithError error: NSError!) {
+        NSLog("bannerView")
+    }
+    
+    //... your class implementation code
+    
 }
