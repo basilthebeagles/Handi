fb242e7e4072c1c6d59b50ef32930c05f34a9329
basilthebeagles <johnjoestack@gmail.com>
2015-10-01 14:06:02 +0100

    Options screen is good now

---
+++ Handi/OptionsModel.swift
@@ -0,0 +1,38 @@
+//
+//  OptionsModel.swift
+//  Bill Splitter
+//
+//  Created by JohnJoe Stack on 29/09/2015.
+//  Copyright © 2015 FStack. All rights reserved.
+//
+
+import Foundation
+
+
+
+class OptionsModel{
+    
+    
+    
+    func peformTableAction(){
+        
+        
+        //etc open links or initate in app purchases.
+        /*
+
+UIApplication.sharedApplication().openURL(NSURL(string : "itms-apps://itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?id=\\()&onlyLatestVersion=true&pageNumber=0&sortOrdering=1)")!);
+
+*/
+        
+        
+    }
+    
+    
+    
+    init(){
+        
+    }
+    
+    
+    
+}
--- Handi/OptionsTableViewController.swift
+++ Handi/OptionsTableViewController.swift
@@ -1,41 +1,112 @@
 //
-//  SettingsTableViewController.swift
+//  OptionsTableViewController.swift
 //  Bill Splitter
 //
-//  Created by JohnJoe Stack on 26/09/2015.
+//  Created by JohnJoe Stack on 29/09/2015.
 //  Copyright © 2015 FStack. All rights reserved.
 //
 
 import UIKit
 
-class OptionsTableViewController: UIViewController, UITableViewDelegate{
+class OptionsTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
 
     override func viewDidLoad() {
         super.viewDidLoad()
-       
         
-        // Do any additional setup after loading the view.
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+    // MARK: - Table view data source
+
+     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 3
+    }
+
+     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 1
+    }
+
     
-  
-    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        print("row touched")
+    
+    
+    
+    
+    
+     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        print("tableview bit")
+        print(indexPath.row)
+        print(indexPath.section)
         
+        
+        
+        var cell: UITableViewCell
+
+        
+        if indexPath.section == 0{
+            print("row 0")
+            cell = tableView.dequeueReusableCellWithIdentifier("disableAdvertsCell", forIndexPath: indexPath)
+        }else if indexPath.section == 1{
+            print("row 1")
+            cell = tableView.dequeueReusableCellWithIdentifier("rateOurAppCell", forIndexPath: indexPath)
+        }else if indexPath.section == 2{
+            print("row 2")
+            cell = tableView.dequeueReusableCellWithIdentifier("supportCell", forIndexPath: indexPath)
+        }else{
+            cell = tableView.dequeueReusableCellWithIdentifier("ERROR", forIndexPath: indexPath)        }
+        
+        // Configure the cell...
+        
+
+        return cell
     }
     
-    
-    
-    
-    func rateApp(){
-    UIApplication.sharedApplication().openURL(NSURL(string : "itms-apps://itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?id=\\()&onlyLatestVersion=true&pageNumber=0&sortOrdering=1)")!);
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
     }
-    
-    
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
     /*
     // MARK: - Navigation
 
